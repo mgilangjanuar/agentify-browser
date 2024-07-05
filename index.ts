077120bdc5a60ab3762c7863786ec262f4fa9429
M Gilang Januar <mgilangjanuar@gmail.com>
2024-07-05 16:22:13 +0700

    chore: Add authorization and screenshot functionality to fetch endpoint

--- index.ts
+++ index.ts
@@ -1,7 +1,37 @@
+import { Playwright } from './lib/playwright'
+
 const server = Bun.serve({
   port: process.env.PORT || 4013,
-  fetch(request) {
-    return new Response("Welcome to Bun!")
+  async fetch(req: Request) {
+    const secret = req.headers.get('Authorization')
+    if (secret !== `Bearer ${process.env.SECRET}`) {
+      return new Response(JSON.stringify({}), {
+        status: 401,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+    }
+
+    const body = await req.json() as { url: string }
+    if (!body.url) {
+      return new Response(JSON.stringify({}), {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+    }
+
+    const browser = await new Playwright(body.url).launch()
+    const buffer = await browser.getScreenshot(true)
+    await browser.close()
+    return new Response(buffer, {
+      status: 200,
+      headers: {
+        'Content-Type': 'image/png',
+      }
+    })
   },
 })
 
